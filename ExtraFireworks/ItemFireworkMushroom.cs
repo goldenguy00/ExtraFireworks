a70507831f41b934364ca499a0c0a325cae039ed
kluberge <kibiklu@gmail.com>
2022-06-30 22:17:32 +1000

    tweak mushroom desc again

--- ExtraFireworks/ItemFireworkMushroom.cs
+++ ExtraFireworks/ItemFireworkMushroom.cs
@@ -67,7 +67,7 @@ public class ItemFireworkMushroom : FireworkItem
 
     public override string GetItemDescription()
     {
-        return $"Whenever you <style=cIsUtility>stand still</style>, fire a stream of fireworks starting at about <style=cIsDamage>{scaler.GetValue(1) * 100:0}%</style> speed. <style=cStack>Fire speed increases hyperbolically</style> with number of stacks, starting with <style=cStack>+{(scaler.GetValue(2) - scaler.GetValue(1)) * 100:0}%</style> and going <style=cStack>up to {scaler.Base * 100:0}%</style> speed.";
+        return $"Whenever you <style=cIsUtility>stand still</style>, fire a stream of fireworks starting at about <style=cIsDamage>{scaler.GetValue(1) * 100:0}%</style> speed. <style=cStack>Fire speed increases hyperbolically</style> with number of stacks, starting with <style=cStack>+{(scaler.GetValue(2) - scaler.GetValue(1)) * 100:0}%</style> and going <style=cStack>up to {scaler.Base * 100:0}% speed</style>.";
     }
 
     public override string GetItemLore()
