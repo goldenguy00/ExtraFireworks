51db1fae67a09d56779d4471c1533ced414c709b
kluberge <kibiklu@gmail.com>
2023-02-21 20:43:18 -0600

    add R2API as a dependency and maybe fix NPE issue with items being disabled?

--- ExtraFireworks/ExtraFireworks.cs
+++ ExtraFireworks/ExtraFireworks.cs
@@ -56,7 +56,7 @@ namespace ExtraFireworks
                     return;
 
                 foreach (var item in items)
-                    if (self.pickupIndex.pickupDef != null
+                    if (item.IsEnabled() && self.pickupIndex.pickupDef != null
                         && self.pickupIndex.pickupDef.itemTier == item.Item.tier
                         && self.pickupIndex.pickupDef.itemIndex == item.Item.itemIndex)
                     {
